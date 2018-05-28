28cb885e3fd36ba7d3802e495cfd88f891ba6773
bdebever <baptiste.debever@gmail.com>
2018-05-28 13:12:36 +0200

    Changes on the program - added library

--- Programme.ino
+++ Programme.ino
@@ -1,4 +1,4 @@
-      /**
+/**
  * Projet Arduino IE4 - asservissement d'un robot
  * @file    Programme.ino
  * @author  Baptiste Debever - Alexandre Deledalle
@@ -13,34 +13,41 @@
 #include "Arduino.h"
 #include "SimpleTimer.h"
 
-SimpleTimer timer;                 // Timer pour échantillonnage
-unsigned int tick_codeuse = 0;     // Compteur de tick de la codeuse
+unsigned int    tickCodeuse = 0;     // Compteur de tick de la codeuse
+boolean         entryFound = false;
+boolean         exitFound = false;
+int             distance = 0;
+int             axeCentre = 0; // Decalage par rapport au centre
+unsigned long   temps_attente;
+float           speed1, speed2, speed3;
+bool            debug = false;
+bool            graph = true;
+float           erreur;
+float           somme_erreurs;
+float           erreur_precedente;
+unsigned long   valeur_mesuree;
+int16_t         moveSpeed = 10;
+const int       frequenceEchantillonnage = 50;
+int             consigne = 10;
+float           commande = 0;
 
-boolean entryFound = false;
-boolean exitFound = false;
-int distance = 0;
-int axeCentre = 0; // Decalage par rapport au centre
+int             pixySignature;
+int             pixyX = 0;
 
 // la liste des états possible de notre système
 enum {FORWARD, SIDEWARD, STANDSTILL} direction;
 
-unsigned long temps_actuel;
-unsigned long mem_temps;
-
-// Distance parcourue en 1 tour moteur
-const float distanceMotorLapse = 26.8;
-
 // Motors setup
-int16_t moveSpeed = 50;
 MeEncoderOnBoard Encoder_1(PORT_1);
 MeEncoderOnBoard Encoder_2(PORT_2);
 MeEncoderOnBoard Encoder_3(PORT_3);
 
-/* Ultrasonic setup */
+// Capteur ultrasonor
 MeUltrasonicSensor *us = NULL;     //PORT_7
 
 // Pixy setup
 Pixy pixy;
+SimpleTimer timer;
 
 /// Interruption pour encodeur1
 void isr_process_encoder1(void)
@@ -91,6 +98,11 @@ void setup()
   attachInterrupt(Encoder_3.getIntNum(), isr_process_encoder3, RISING);
   Serial.begin(115200);  // Init communication série (USB-série)
 
+  attachInterrupt(0, compteur, CHANGE);
+
+  // Mise en place de l'asservissement
+  timer.setInterval(1000 / frequenceEchantillonnage, asservissement);
+
   //Set PWM à 1khz
   TCCR1A = _BV(WGM10);//PIN12
   TCCR1B = _BV(CS11) | _BV(CS10) | _BV(WGM12);
@@ -104,9 +116,12 @@ void setup()
   TCCR4A = _BV(WGM40);//PIN5
   TCCR4B = _BV(CS41) | _BV(CS40) | _BV(WGM42);
   // Fin Set PWM
+  
+  // Pixy et Ultrason setup
   pixy.init();
-
   us = new MeUltrasonicSensor(PORT_7); // Init Ultrason
+
+  direction = FORWARD;
 }
 
 /**
@@ -114,17 +129,8 @@ void setup()
  */
 void loop()
 {
-  // Variables locales
-  float speed1, speed2, speed3;
-
-  //--- Attente ---//
-  // Réalisation d'une attente avec la resynchronisation du programme
-  do
-  {
-    temps_actuel = millis() ;
-  }
-  while((temps_actuel-mem_temps)<=temps_attente);  //
-  mem_temps = temps_actuel;
+  // On lance le timer
+  timer.run();
 
   // Lecture encodeurs
   Encoder_1.loop();
@@ -134,184 +140,46 @@ void loop()
   speed2 = Encoder_2.getCurrentSpeed();
   speed3 = Encoder_3.getCurrentSpeed();
 
+  // Demarrer moteurs
+  //Encoder_1.moveTo(50);
+  //Encoder_3.moveTo(50);
+  
+  // Debugs
+  if (debug) {
+    Serial.print("Distance : ");
+    Serial.print(us->distanceCm());
+    Serial.println(" cm");
+    Serial.print("Position moteur 1 : ");
+    Serial.println(Encoder_1.getCurPos());
+    Serial.print("PixyX :");
+    Serial.println(pixyX);
+  }
+
+  /**
+   * On trace le graph
+   */
+  if (graph) {
+    Serial.print(consigne);
+    Serial.print(", ");
+    Serial.print(Encoder_1.getCurrentSpeed());
+    Serial.print(", ");
+    Serial.println();
+    delay(10);
+  }
+
   // On interroge l'ultrason
   getUltrasensorValue();
 
-  // On avance
-  Encoder_1.setMotorPwm(-moveSpeed);
-  Encoder_3.setMotorPwm(-moveSpeed);
-  direction = FORWARD;
-
-  //--- Machine d'etat - similaire a un Grafcet ---//
-  // Comme un Grafcet
-
-  switch(direction)
-  {
-      case FORWARD:
-        // Distance superieure a 20 - aucun besoin de s'inquieter
-        if (distance > 20)
-          break;
-
-        // On arrete le moteur car distance inferieure a 20
-        stopMotor();
-        direction = STANDSTILL;
-        break;
-
-      case SIDEWARD:
-
-        // Rien en vue -> on peut se decaler vers la droite
-        if (distance > 20) {
-          forward(true);
-          axeCentre += 1;
-          // On se replace vers lavant
-          turnLeft(90);
-        } else {
-          turnLeft(180);
-          // On suppose donc quil ny a rien
-          forward(true);
-          axeCentre -= 1;
-          turnRight(90);
-        }
-
-        // On repart de lavant
-        forward(false);
-        // Set le nouvel etat
-        direction = FORWARD;
-        break;
-
-      case STANDSTILL:
-
-        // Demander a Pixy si on detecte un object connu
-        if (getPixyValue()) {
-
-          // Sil sagit de la porte dentree (on suppose rien dautre pour linstant)
-          // se decaler
-
-          // Sil sagit de la porte de sortie
-
-          // Stop
-        } else {
-          // Sinon, on regarde sur la droite
-          turnRight(90);
-          getUltrasensorValue();
-          delay(100);
-
-        }
-
-        // Set le nouvel etat
-        direction = SIDEWARD;
-        break;
-  }
-
-
-  //--- Ecritures des valeurs (Print) ---//
-
-  Serial.print("Distance:");
-  Serial.print(distance);
-  Serial.print("Megapi:");
-  Serial.print(distance);
-
-  //--> Ecriture pour le traceur série
-  Serial.print(speed1);
-  Serial.print(", ");
-  Serial.print(speed2);
-  Serial.print(", ");
-  Serial.print(speed3);
-
-  // We don't have the ball: get the distance with the Pixy
-  if (!entryFound) {
-    Serial.print("ball: ");
-    Serial.println(entryFound);
-
-
-    Serial.print(Encoder_1.getCurrentSpeed());
-    Serial.print(Encoder_3.getCurrentSpeed());
-
-    if (getUltrasensorValue() < 5) {
-        entryFound = true;
-        Serial.println(Encoder_1.getCurrentSpeed());
-    }
-
-
-  }
-
-
 }
 
 
 /**
- * Fonction permettant de se tourner vers la droite
- *
- * @var motor - la rotation
+ * Arreter les moteurs
  */
-void turnRight(int rotation)
-{
-  Encoder_1.setMotorPwm(-rotation);
-  Encoder_3.setMotorPwm(+rotation);
-  delay(500);
-  Encoder_1.setMotorPwm(0);
-  Encoder_3.setMotorPwm(0);
-}
-
-/**
- * Fonction permettant de se tourner vers la gauche
- *
- * @var motor - la rotation
- */
-void turnLeft(int rotation)
-{
-  Encoder_1.setMotorPwm(+rotation);
-  Encoder_3.setMotorPwm(-rotation);
-  delay(500);
-  Encoder_1.setMotorPwm(0);
-  Encoder_3.setMotorPwm(0);
-}
-
-/**
- * Avancer d'une vitesse
- */
-void forward()
-{
-  // Distance to go in degrees (360 = 1 lapse)
-  //int distanceToGo = ((distanceGo * 360) / distanceMotorLapse);
-  Encoder_1.setMotorPwm(-80*1.2);
-  Encoder_3.setMotorPwm(-80);
-  delay(600);
-  Encoder_1.setMotorPwm(0);
-  Encoder_3.setMotorPwm(0);
-}
-
-void stopMotor()
+void stopMotor(void)
 {
   Encoder_1.setMotorPwm(0);
   Encoder_3.setMotorPwm(0);
-}
-
-/**
- * Fonction permettant de get la vitesse du moteur
- */
-void getMotorSpeed()
-{
-  Serial.print("Moteur 1: ");
-  Serial.println(Encoder_1.getCurrentSpeed());
-  Serial.print("Moteur 2: ");
-  Serial.println(Encoder_2.getCurrentSpeed());
-  Serial.print("Moteur 3: ");
-  Serial.println(Encoder_3.getCurrentSpeed());
-}
-
-/**
- * Fonction permettant de tracer une courbe (eg: position, vitesse en fonction du temps)
- */
-void draw()
-{
-  // Lecture des encodeurs
-  Encoder_1.loop();
-  Encoder_2.loop();
-  Encoder_3.loop();
-  Serial.print(Encoder_1.getCurrentSpeed());
-  delay(10);
-  Serial.println();
 }
 
 /**
@@ -324,7 +192,7 @@ float getUltrasensorValue()
 }
 
 /**
- *
+ * Récuperer valeurs pixy
  */
 void getPixyValue()
 {
@@ -336,71 +204,266 @@ void getPixyValue()
   // grab blocks!
   blocks = pixy.getBlocks();
 
-  // If there are detect blocks, print them!
   if (blocks)
   {
     i++;
 
-    // do this (print) every 50 frames because printing every
-    // frame would bog down the Arduino
-    if (i%50==0)
+    // Chaque 5 frames
+    if (i % 5 == 0)
     {
-      sprintf(buf, "Detected %d:\n", blocks);
-      Serial.print(buf);
-      for (j=0; j<blocks; j++)
-      {
-        sprintf(buf, "  block %d: ", j);
+
+      if (debug) {
+        sprintf(buf, "Detected %d:\n", blocks);
         Serial.print(buf);
-        pixy.blocks[j].print();
       }
+
+      // Pixel resolution --> 320 * 200, donc center à 160
+      int minX = 160;
+
+      for (j = 0; j < blocks; j++)
+      {
+
+        if (debug) {
+          sprintf(buf, "  block %d: ", j);
+          Serial.print(buf);
+          pixy.blocks[j].print();
+          Serial.print("\n");
+        }
+
+        pixySignature = pixy.blocks[j].signature;
+        pixyX = pixy.blocks[j].x;
+      }
+
     }
   }
-}
-
-
-////**************** Fonctions relatives au PID ***************//////
-/* Interruption sur tick de la codeuse */
-void compteur(){
-    tick_codeuse++;  // On incrémente le nombre de tick de la codeuse
+   
 }
 
 
 /**
- * Interruption pour calcul du PID
- * @link http://www.ferdinandpiette.com/blog/2012/04/asservissement-en-vitesse-dun-moteur-avec-arduino/
- */
-void asservissement(float consigne, int moteur)
+   \par Function
+      Forward
+   \par Description
+      This function use to control the car kit go forward.
+*/
+void Forward(void)
 {
-    // DEBUG
-    Serial.println(tick_codeuse);
+  Encoder_1.setMotorPwm(moveSpeed);
+  Encoder_3.setMotorPwm(moveSpeed);
+}
 
-    // Réinitialisation du nombre de tick de la codeuse
-    tick_codeuse=0;
+/**
+   \par Function
+      Backward
+   \par Description
+      This function use to control the car kit go backward.
+*/
+void Backward(void)
+{
+  Encoder_1.setMotorPwm(-moveSpeed);
+  Encoder_3.setMotorPwm(-moveSpeed);
+}
+
+/**
+   \par Function
+      TurnLeft
+   \par Description
+      This function use to control the car kit go backward and turn left.
+*/
+void TurnLeft(void)
+{
+  Encoder_1.setMotorPwm(moveSpeed);
+  Encoder_3.setMotorPwm(moveSpeed / 2);
+}
+
+/**
+   \par Function
+      TurnLeft
+   \par Description
+      This function use to control the car kit go backward and turn left.
+*/
+void TurnRight(void)
+{
+  Encoder_1.setMotorPwm(moveSpeed / 2);
+  Encoder_3.setMotorPwm(moveSpeed);
+}
+
+/**
+   \par Function
+      ReverseRight
+   \par Description
+      This function use to control the car kit go backward and turn right.
+*/
+void ReverseRight(void)
+{
+  Encoder_1.setMotorPwm(-moveSpeed);
+  Encoder_3.setMotorPwm(moveSpeed);
+}
+
+/**
+   \par Function
+      TurnRight
+   \par Description
+      This function use to control the car kit go backward and turn right.
+*/
+void ReverseLeft(void)
+{
+  Encoder_1.setMotorPwm(moveSpeed);
+  Encoder_3.setMotorPwm(-moveSpeed);
+}
+
+/*
+   Compteur pour le PID et l'asservisement
+*/
+void compteur() {
+  tickCodeuse++;
+}
+
+/**
+ * Interruption pour calcul du PID
+ */
+void asservissement()
+{
+    float result_P;
+    float result_D;
+    float result_I;
+    float Kp = 0.66;
+    float Ki = 0.7;
+    float Kd = 0;
+    float delta_erreurs;
+    int minX = 160;
+
+   int moteur = 1;
+
+    switch(moteur)
+    {
+        case 1:
+          valeur_mesuree = speed1;
+          break;
+        case 2:
+          valeur_mesuree = speed2;
+        case 3:
+          valeur_mesuree = speed3;
+          break;
+    }
 
     //--- PID ---//
-    erreur = consigne - valeur_mesuree;
-
+    erreur = consigne - Encoder_1.getCurrentSpeed();;
+    
     result_P = erreur * Kp;
     somme_erreurs +=  erreur;
+    
     result_I = somme_erreurs * Ki;
-    delta_erreur = erreur - erreur_precedente;
+    delta_erreurs = erreur - erreur_precedente;
+    
     erreur_precedente = erreur;
     result_D = delta_erreurs * Kd;
 
     // Notre commande
     commande = result_P + result_I + result_D;
-    // saturation
-    commande = constrain(commande,-255,255);
+       
+    // On assigne la valeur aux moteurs
+    moveSpeed = commande;
 
-    // Reguler
-    switch(moteur)
-    {
-        case 1:
-          Encoder_1.setMotorPwm(commande);
-          break;
-        case 2:
-          Encoder_2.setMotorPwm(commande);
-        case 3:
-          Encoder_3.setMotorPwm(commande);
+    // Appeler Pixy 
+    getPixyValue();
+
+  
+  // Si présence drapeau: arret
+    if (pixySignature == 1 && 
+      minX - 50 <= pixyX <= minX + 50) {
+      // Carton rouge: stop
+      stopMotor();
+      while(1){};// Stop
     }
+
+    while(pixySignature == 2) {
+      if(pixyX < 160) {
+        TurnRight();
+      } else {
+        TurnLeft();
+      }
+  
+      getUltrasensorValue();
+  
+      // Eviter obstacle intermédiaire
+      if (distance < 30)
+        break;
+      
+      // Refresh;
+      getPixyValue();
+    }
+
+    if (distance > 30) {
+      Forward();
+      // On a un objet (joueur)  
+    } else {
+      // Sinon, on tourne sur la droite
+      if (debug)
+        Serial.println("Obstacle detecte");
+      
+      Backward();
+      delay(500);
+      //Forward();
+      delay(2000);
+      TurnLeft();
+      delay(200);
+      
+    }
+  
+  /*
+  switch(direction)
+  {
+      case FORWARD:
+        // Distance superieure a 20 - aucun besoin de s'inquieter
+        if (distance > 20) {
+          Forward();
+          break;
+        }
+
+        // On arrete le moteur car distance inferieure a 20
+        stopMotor();
+        direction = STANDSTILL;
+        break;
+
+      case SIDEWARD:
+
+        // Rien en vue -> on peut se decaler vers la droite
+        if (distance > 20) {
+          Forward();
+          delay(200);
+          stopMotor();
+          // On se replace vers lavant
+          TurnLeft();
+        } else {
+          TurnLeft();
+          // On suppose donc quil ny a rien
+          Forward();
+          axeCentre -= 1;
+          TurnRight();
+        }
+
+        // Set le nouvel etat
+        direction = FORWARD;
+        break;
+
+      case STANDSTILL:
+
+        // Demander a Pixy si on detecte un object connu
+        if (getPixyValue()) {
+          // Sil sagit de la porte de sortie: stop
+          stopMotor();
+          while(1){};
+          // Stop
+        } else {
+          // Sinon, on regarde sur la droite
+          TurnRight();
+          delay(100);
+        //}
+
+        // Set le nouvel etat
+        direction = SIDEWARD;
+        break;
+  }*/
 }
+
